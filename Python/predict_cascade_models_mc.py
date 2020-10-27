9fceb5a3389c14fc751ef697e59cf0df5e0fc922
Wafa Anam <wafa.anam@ucalgary.ca>
2020-10-27 12:56:50 -0600

    add combined k-space data generator

---
+++ Python/predict_cascade_models_mc.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+import numpy as np
+import matplotlib.pylab as plt
+import os
+import glob
+import sys
+import natsort
+from tensorflow.keras.optimizers import  Adam
+import nibabel as nib
+import h5py
+import tensorflow as tf
+
+# Include path to my modules
+MY_UTILS_PATH = "../Modules/"
+if not MY_UTILS_PATH in sys.path:
+    sys.path.append(MY_UTILS_PATH)
+
+# Import my modules
+import cs_models_mc as fsnet
+
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+
+# In[2]:
+
+
+# Input parameters
+channels = 24
+batch_size = 8
+weights_path = "../Models/weights_wwnet_ikik_mc_r5.h5"
+model_string = "ikik"
+test_path = "../../data/test_12_channel/Test-R=5/"
+results_path = "../WWnet-ikik-baseline/12-channel-R=5/"
+cascade = "unet"
+crop = (50,-50) # slices to crop
+verbose = True
+
+
+# In[3]:
+
+
+test_files = np.asarray(glob.glob(test_path + "*.h5"))
+
+# Sort by kx - ky size
+files_sizes = []
+for ii in test_files:
+    with h5py.File(ii, 'r') as f:
+        kspace_test = f['kspace']
+        files_sizes.append(kspace_test.shape[1]*kspace_test.shape[2]) 
+files_sizes = np.asarray(files_sizes)
+indexes = np.argsort(files_sizes)
+test_files = test_files[indexes]
+
+if verbose:
+    print(cascade)
+    print("Domains: ", model_string)
+    print("Number of files:", len(test_files))
+    print(test_files[0])
+    print("Weights path:", weights_path)
+    print("Test path:", test_path)
+    print("results path:", results_path)
+    
+norm = np.sqrt(218*170)
+model_exists = False
+for ii in test_files:
+
+    name = ii.split("/")[-1]
+    # Load data
+    with h5py.File(ii, 'r') as f:
+        kspace_test=  np.array(f.get('kspace')).astype(np.float32)[crop[0]:crop[1]]
+    
+    kspace_test = kspace_test/norm
+
+    Z,H,W,_ = kspace_test.shape
+    if H*W != (218*170): 
+        model_exists = False
+
+    Wpad = (8 -W%8)//2
+    Hpad = (8 -H%8)//2
+
+    if not model_exists:
+        if cascade == 'unet':
+            model = fsnet.deep_cascade_unet(depth_str = model_string, H = H, W = W, Hpad = Hpad, Wpad = Wpad, channels = channels) 
+        elif cascade == 'flat':
+            model = fsnet.deep_cascade_flat_unrolled(depth_str = model_string, H = H,W = W,depth = 14, kshape = (3,3), nf = 116,channels = channels)              
+
+        model_exists = True
+        opt = Adam()
+        model.compile(loss = 'mse',optimizer = opt)
+        model.load_weights(weights_path)   
+
+    var_sampling_mask = ((kspace_test == 0)).astype(np.float32)
+    pred = model.predict([ kspace_test, var_sampling_mask],batch_size = batch_size)*norm
+    pred = pred[:,:,:,::2]+1j*pred[:,:,:,1::2]
+    pred = np.sqrt((np.abs(pred)**2).sum(axis = -1)) # Root sum of squares
+
+    if verbose:
+        print(os.path.join(results_path,name))
+    with h5py.File(os.path.join(results_path,name), 'w') as hf:
+        hf.create_dataset('reconstruction', data=pred)
+
---
+++ Python/train_cascade_models_mc.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Importing standard libraries
+import numpy as np
+import os
+import glob
+import sys
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
+from tensorflow.keras.optimizers import  Adam
+from tensorflow.keras.models import load_model
+import pickle
+import tensorflow as tf
+
+# Include path to my modules
+MY_UTILS_PATH = "../Modules/"
+if not MY_UTILS_PATH in sys.path:
+    sys.path.append(MY_UTILS_PATH)
+
+# Import my modules
+import cs_models_mc as fsnet
+from data_generator import DataGenerator
+
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+
+# In[2]:
+
+
+# Input parameters
+H = 218
+W = 170
+channels = 24
+batch_size = 8
+epochs = 100
+lr = 1e-4
+patience = 5
+sampling_mask_path = "../Data/poisson_sampling/R5_218x170.npy"
+model_path =  "../Models/model_wwnet_ikik_mc_r5.h5"
+weights_path = "../Models/weights_wwnet_ikik_mc_r5.h5"
+model_string = "ikik"
+train_path = "../../data/train_val_12_channel/Train/"
+val_path = "../../data/train_val_12_channel/Val/"
+cascade = "unet"
+nslices = 256
+crop = (15, 15)
+verbose = True
+
+
+# In[ ]:
+
+
+train = glob.glob(train_path + "*.h5")
+val = glob.glob(val_path + "*.h5")
+
+# undersampling patterns - centered k-space - 100 pre computed patterns
+var_sampling_mask = np.load(sampling_mask_path)
+
+
+if verbose:
+    print(cascade)
+    print("Domains: ", model_string)
+    print("Model name: ", model_path)
+    print("Weights path: ", weights_path)
+    print("Sampling:", 1.0*var_sampling_mask.sum() / (var_sampling_mask.size*0.85)) # 85 % sampling Kz direction
+    print("Train path: ", train_path)
+    print("Validation path: ", val_path)
+
+
+# Replicate sampling mask across number of channels
+var_sampling_mask = np.repeat(var_sampling_mask[:, :, :, np.newaxis], 24, axis = -1) 
+
+train_generator = DataGenerator(train, dim = (H, W), under_masks = var_sampling_mask,  crop = crop,                            batch_size = batch_size, n_channels = channels, nslices = nslices, shuffle=True)
+
+val_generator = DataGenerator(val, dim = (H, W), under_masks = var_sampling_mask,  crop = crop,                            batch_size = batch_size, n_channels = channels, nslices= nslices, shuffle=False)
+
+
+
+# Callbacks
+earlyStopping = EarlyStopping(monitor='val_loss',
+                                           patience=patience, 
+                                           verbose=0, mode='min')
+
+checkpoint = ModelCheckpoint(weights_path, mode = 'min',                              monitor='val_loss',verbose=0,                             save_best_only=True, save_weights_only = True)
+
+checkpoint2 = ModelCheckpoint(model_path, monitor='val_loss', verbose=0, save_best_only=False,                save_weights_only=False, mode='min')
+
+Hpad = (8 - (H%8)) // 2
+Wpad = (8 - (W%8)) // 2
+if os.path.isfile(model_path):
+    model = load_model(model_path)
+elif  cascade == 'unet':
+    model = fsnet.deep_cascade_unet(depth_str = model_string, H = H, W = W, Hpad = Hpad, Wpad = Wpad, channels = channels) 
+elif cascade == 'flat':
+    model = fsnet.deep_cascade_flat_unrolled(depth_str = model_string, H = H,W = W,depth = 6, kshape = (3,3), nf = 64,channels = channels)
+else:
+   pass
+
+if not os.path.isfile(model_path):
+    opt = Adam(learning_rate = lr)
+    model.compile(loss = 'mse',optimizer=opt)
+
+if verbose:
+    print(model.summary())
+
+hist = model.fit(train_generator, epochs = epochs, steps_per_epoch = ( (nslices - crop[0] - crop[1])*len(train) ) // batch_size,              validation_data = val_generator, validation_steps = ( (nslices - crop[0] - crop[1])*len(val) ) // batch_size,                    callbacks = [ checkpoint2, checkpoint, earlyStopping ], verbose = 1)
+
+# Save model history
+with open(model_path[:-2] + "history", 'wb') as file_pi:
+    pickle.dump(hist.history, file_pi)
+
+
